a79998a994a8ca2823ef7afb2a19c6188fe67c34
Chxrifin <83019212+ChareefHayeemadeng@users.noreply.github.com>
2024-10-12 00:07:21 +0700

    update all files

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,126 @@
 /*   By: chayeema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:11:42 by chayeema          #+#    #+#             */
-/*   Updated: 2024/01/11 14:11:43 by chayeema         ###   ########.fr       */
+/*   Updated: 2024/10/12 00:05:23 by chayeema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void	clean(t_list **list)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+	t_list	*last_node;
+	t_list	*next_node;
+
+	tmp = malloc(BUFFER_SIZE + 1);
+	if (!tmp)
+		return ;
+	next_node = malloc(sizeof(t_list));
+	if (!next_node)
+		return ;
+	last_node = check_tail(*list);
+	i = 0;
+	j = 0;
+	while (last_node->lstbuf[i] && last_node->lstbuf[i] != '\n')
+		i++;
+	while (last_node->lstbuf[i] && last_node->lstbuf[i++])
+		tmp[j++] = last_node->lstbuf[i];
+	tmp[j] = '\0';
+	next_node->lstbuf = tmp;
+	next_node->next = NULL;
+	ft_free(list, tmp, next_node);
+}
+
+char	*getstr(t_list *list)
+{
+	int		len;
+	char	*next_str;
+
+	if (!list)
+		return (NULL);
+	len = ft_strlen(list);
+	next_str = malloc(len + 1);
+	if (!next_str)
+		return (NULL);
+	cpstr(list, next_str);
+	return (next_str);
+}
+
+void	addtolst(t_list **list, char *char_buf)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	last_node = check_tail(*list);
+	if (!last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->lstbuf = char_buf;
+	new_node->next = NULL;
+}
+
+void	create_list(t_list **list, int fd)
+{
+	char	*char_buf;
+	int		read_line;
+
+	while (!check_newline(*list))
+	{
+		char_buf = malloc(BUFFER_SIZE + 1);
+		if (!char_buf)
+			return ;
+		read_line = read(fd, char_buf, BUFFER_SIZE);
+		if (read_line < 0)
+		{
+			free(char_buf);
+			return ;
+		}
+		if (!read_line)
+		{
+			free(char_buf);
+			return ;
+		}
+		char_buf[read_line] = '\0';
+		addtolst(list, char_buf);
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list = NULL;
+	char			*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (!list)
+		return (NULL);
+	next_line = getstr(list);
+	clean(&list);
+	return (next_line);
+}
+/*
+#include <stdio.h>
+# include <fcntl.h>
+int	main()
+{
+	int		fd;
+	char	*str;
+	int		count;
+
+	count= 1;
+	fd = open("main", O_RDONLY);
+	while ((str = get_next_line(fd)))
+	{
+		printf("%d -> %s\n", count++, str);
+		free(str);
+	}
+}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,30 @@
 /*   By: chayeema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:11:51 by chayeema          #+#    #+#             */
-/*   Updated: 2024/01/11 14:11:52 by chayeema         ###   ########.fr       */
+/*   Updated: 2024/10/12 00:05:23 by chayeema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	char			*lstbuf;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(t_list *list);
+int		check_newline(t_list *list);
+t_list	*check_tail(t_list *list);
+void	cpstr(t_list *list, char *str);
+void	ft_free(t_list **list, char *tmp, t_list *next_node);
+#endif
